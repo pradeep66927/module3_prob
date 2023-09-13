0af92b75d83f6747b7680a46267fd2041a245fdc
iampradeep <91717371+pradeep66927@users.noreply.github.com>
2023-09-13 15:20:29 +0530

    second commit2

--- Module_3_LOOP/FC119.py
+++ Module_3_LOOP/FC119.py
@@ -1,8 +1,9 @@
-p=int(input("lower : "))
-q=int(input(" upper :"))
-s=0
-for i in range(p,q+1):
-    if i%p==0:
-        if  i%q !=0:
-            s+=i
-print(s)
+n=int(input('enter number : '))
+for i in range(1,n+1):
+    if n%i == 0:
+        c=0
+        for j in range(1,i+1):
+            if i%j == 0:
+                c+=1
+        if c == 2:
+            print(i)
